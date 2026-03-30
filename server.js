ccf2f2689857c3fe4d047511f8cc41fcef3d9b35
TALITA <josephtalita@gmail.com>
2026-03-29 23:06:15 -0300

    atualizando frontend

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const openai = new OpenAI({
 });
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
